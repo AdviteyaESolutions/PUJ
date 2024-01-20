ebeda9e440442dfeedd24a841fdad7a3ff6dc857
Rohit Jain <rohit@fullstacklearning.com>
2024-01-20 17:35:15 +0530

    class 20-01-2024

---
+++ javascript/index.js
@@ -0,0 +1,130 @@
+document.querySelector("button").onclick = () => {
+  alert("Hello World");
+};
+
+// VARIABLES: vary + able
+
+// int a
+// float b
+// double c
+// char n
+
+// var a = true
+// let b = 10
+
+// DIFFERENCE BETWEEN VAR & LET
+
+// 1. Security Leak
+// 2. Memory Leak
+
+// {
+//     let  a = 10;
+// }
+
+// {
+// 	let a = 1;
+// }
+
+// var a = 1;
+
+// var a = 2;
+
+// let a = 1;
+
+// let a = 2;
+
+// let a = 10
+
+// let a = 1;
+// a = 2;
+
+// const a = 1;
+// a = 2;
+
+// let a = 30;
+
+// var b = 100;
+
+// var b = 200;
+
+//OPERATORS
+
+// let a = 1;
+// let b = 2;
+
+// console.log(a > b)
+
+// let a = 1;
+// let b = 2;
+
+//console.log(a > b)
+// console.log(a <= b)
+
+// == checks only for the values on both sides
+// === checks for the values as well as data type on both sides
+
+// console.log("11" == "10")
+// console.log(10 !== "10")
+
+// let a = 0;
+// a++;
+// a--;
+// --a;
+// a++;
+// console.log(a);
+
+let x = 0;
+console.log(x++); //0
+console.log(--x); //0
+console.log(x--); //0
+console.log(++x); //0
+
+// for(let i=0;i<=10;i++){
+// }
+// console.log(i);
+
+// var i = null;
+// while (i) {
+//   console.log(i);
+//   i++;
+// }
+
+// let i=1;
+// do{
+// 	console.log(i++)
+// }while(i>=10)
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// for (let i = 4; i >= 0; i--) {
+//   for (let j = 0; j <= i; j++) {
+//     console.log("*");
+//   }
+//   console.log("<br>");
+// }
+
+// 1514131211
+// 10987
+// 654
+// 32
+// 1
+
+// console.log(1+2+3 + "Apples");
+
+// for (let i = 4; i >= 0; i--) {
+//   for (let j = 0; j <= i; j++) {
+//     document.write("*");
+//   }
+//   document.write("<br>");
+// }
+
+// var a;
+// console.log(a);
+
+// a =5;
+
+
